04f8f0a2d3d8e884821cae214a50207f780cd3b3
Kevin Stone <ruca6616@gmail.com>
2023-02-23 17:18:27 -0500

    added object to Session array

--- index.php
+++ index.php
@@ -9,8 +9,8 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-//Start a session
-session_start();
+
+//var_dump($_SESSION);
 
 //Require files
 require_once('vendor/autoload.php');
@@ -18,13 +18,15 @@ require_once('model/data-layer.php');
 require_once('model/validate.php');
 //require_once('classes/order.php');
 
-// class test code
-$myOrder = new Order();
-$myOrder->setFood("tacos");
-$myOrder->setMeal("salsa");
-$myOrder->setCondiments("taquitos");
+//Start a session, must start Session after requiring auto-load files
+session_start();
 
-var_dump($myOrder);
+// class test code
+//$myOrder = new Order();
+//$myOrder->setFood("tacos");
+//$myOrder->setMeal("salsa");
+//$myOrder->setCondiments("taquitos");
+//var_dump($myOrder);
 
 /*
 $food1 = "tacos";
@@ -68,10 +70,15 @@ $f3->route('GET|POST /order1', function($f3) {
     //If the form has been submitted
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
+        // create object. This object will be stored in the Session array
+        $newOrder = new Order();
+
+
         //Move food from POST array to SESSION array
         $food = trim($_POST['food']);
         if (validFood($food)) {
-            $_SESSION['food'] = $food;
+            $newOrder->setFood($food);
+            //$_SESSION['food'] = $food; we're putting into object now instead of the Session array
         }
         else {
             $f3->set('errors["food"]',
@@ -81,7 +88,8 @@ $f3->route('GET|POST /order1', function($f3) {
         //Validate the meal
         $meal = $_POST['meal'];
         if (validMeal($meal)) {
-            $_SESSION['meal'] = $meal;
+            $newOrder->setMeal($meal);
+            //$_SESSION['meal'] = $meal;
         }
         else {
             $f3->set('errors["meal"]',
@@ -91,7 +99,10 @@ $f3->route('GET|POST /order1', function($f3) {
         //Redirect to summary page
         //if there are no errors
         if (empty($f3->get('errors'))) {
-            $f3->reroute('summary');
+
+            // put newOrder object in the Session array
+            $_SESSION['newOrder'] = $newOrder;
+            $f3->reroute('order2');
         }
     }
 
@@ -110,8 +121,12 @@ $f3->route('GET|POST /order2', function($f3) {
     //If the form has been submitted
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
+        // assign condiments to newOrder object
+        $condimentString = implode(", ",$_POST['conds']);
+        $_SESSION['newOrder']->setCondiments($condimentString);
+
         //Move data from POST array to SESSION array
-        $_SESSION['conds'] = implode(", ",$_POST['conds']);
+        //$_SESSION['conds'] = implode(", ",$_POST['conds']);  We're now adding directly to object instead of Session
 
         //Redirect to summary page
         $f3->reroute('summary');
